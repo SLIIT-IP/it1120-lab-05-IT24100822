43ad43847f7ba8254c9311f059e73b40e0b142f0
IT24100822 <IT24100822@my.sliit.lk>
2024-08-21 09:06:02 +0530

    add IT24100822Lab5Q1.java to Q1

---
+++ Q1/IT24100822Lab5Q1.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class IT24100822Lab5Q1
+{
+  public static void main(String[] args)
+  {
+
+     Scanner input=new Scanner(System.in);
+     int no1,no2,no3,max,min;
+
+
+     System.out.print("Enter number1");
+     no1=input.nextInt();
+     System.out.print("Enter number2");
+     no2=input.nextInt();
+     System.out.print("Enter number3");
+     no3=input.nextInt();
+
+
+     if(no1>no2)
+     {
+       max=no1;
+       min=no2;
+     }
+
+     else
+     {
+       max=no2;
+       min=no1;
+     }
+    
+     if(no3>max)
+     {
+        max=no3;
+     }
+    
+     if(no3<min)
+     {
+        min=no3;
+     }
+     System.out.print("max is" +max);
+     System.out.print("min is" +min);
+
+  }
+}
+     
