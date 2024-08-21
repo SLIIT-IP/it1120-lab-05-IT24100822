514abd2c9db42a16e51ddc6e5c31ea2c3d46e7f2
IT24100822 <IT24100822@my.sliit.lk>
2024-08-21 09:46:04 +0530

    add IT24100822Lab5Q2.java to Q2

---
+++ Q2/IT24100822Lab5Q2.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class IT24100822Lab5Q2
+{
+
+  public static void main(String[] args)
+  {
+     Scanner input=new Scanner(System.in);
+     int noofmembers;
+
+     System.out.print("Enter number of members");
+     noofmembers=input.nextInt();
+     
+     if(noofmembers==1)
+     {    
+        System.out.print("you are entitled pen");
+     }
+     
+     else if(noofmembers==2)
+     {
+        System.out.print("you are entitled to umbrella");
+     }
+    
+     else if(noofmembers==3)
+     {
+        System.out.print("you are entitled to bag");
+     }
+
+     else if(noofmembers==4)
+     {
+        System.out.print("you are entitled to Travelling Chair");
+     }
+     else if(noofmembers>=5)
+     {
+       System.out.print("you are entitled to headphone");
+     }
+    
+     else
+     {
+       System.out.print("no prize");
+     }
+
+  }
+}
